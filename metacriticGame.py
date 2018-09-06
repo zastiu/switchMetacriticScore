2394368a10cfeece9a40662409c670aef29d49e8
zastiu <zastiu@hotmail.com>
2018-09-06 18:05:50 +0200

    Update with beat times

--- metacriticGame.py
+++ metacriticGame.py
@@ -10,61 +10,103 @@ import xml.etree.ElementTree as ET
 import platform
 
 __author__ = "zastiu"
-__version__ = "v0.0.7"
+__version__ = "v0.1"
 
-fileOutName = 'z_titleSwitchScore.txt'
+fileOutName = 'z_titleSwitch.txt'
 titleKeysFile = 'titlekeys.txt'
 
-def get_game_scores(game_to_find,list_games):
-    
+def get_game_scores(game_to_find, list_games):
     for game in list_games:
         if translate_game_name(game[0]) == translate_game_name(game_to_find):
-            score = game[1]            
+            score = game[1]
             user_score = game[2]
-            return score,user_score
-    return "-","-"
+            return score, user_score
+    return "-", "-"
+
+def get_game_times(game_to_find, list_games):
+    for game in list_games:
+        if translate_game_name(game[0]) == translate_game_name(game_to_find):
+            main_story = game[1]
+            main_plus_extra = game[2]
+            completionist = game[3]
+            return main_story, main_plus_extra, completionist
+    return "-","-","-"
 
 def translate_game_name(game):
-    dic = [[":",""], ['.',''], ['"',''], ["&",""], ['&amp;',""], ['&amp;',""], ['(EU)',""], ['-',""], ['®',""],['™',""],['!',""],['~',""], ['(CHN/JP/KOR)',""],['FOR NINTENDO SWITCH',""] ,['for Nintendo Switch',""] ,['(JP)',""], ['(US/JP)',""],['(US/EU)',""],['(US)',""], ["'",""], ["(AU)",""],[' ','']]
+    dic = [[":", ""], ['.', ''], ['"', ''], ["&", ""], ['&amp;', ""], ['&amp;', ""], ['(EU)', ""], ['-', ""], ['®', ""],
+           ['™', ""], ['!', ""], ['~', ""], ['(CHN/JP/KOR)', ""], ['FOR NINTENDO SWITCH', ""],
+           ['for Nintendo Switch', ""], ['(JP)', ""], ['(US/JP)', ""], ['(US/EU)', ""], ['(US)', ""], ["'", ""],
+           ["(AU)", ""], [' ', '']]
     game = replace_all(game, dic)
     return game.lower()
+
 
 def replace_all(text, dic):
     for d in dic:
         text = text.replace(d[0], d[1])
     return text
 
+
 def get_list_of_games():
     list_games = []
-    
+
     page = 0
     last_page = False
-    #fOutt = open(fileMetacritic, 'w', encoding="utf-8")
-    while(last_page == False):
-        url = "http://www.metacritic.com/browse/games/score/metascore/all/switch/all?sort=desc&page="+str(page)
-        html_doc = keep_trying_to_get_html(url)
+    # fOutt = open(fileMetacritic, 'w', encoding="utf-8")
+    while (last_page == False):
+        url = "http://www.metacritic.com/browse/games/score/metascore/all/switch/all?sort=desc&page=" + str(page)
+        html_doc = get_to_html(url)
         cont = html_doc.content
-        if str(cont).find("No Results Found")>=0:
+        if str(cont).find("No Results Found") >= 0:
             last_page = True
         else:
-            split1 =  str(cont).split('body_wrap">')[1]
-            split2 =  split1.split('<div class="post_foot">')[0]
-            tree = split2.replace("\\n","").replace("\\t","").replace("  ","").replace("&","&amp;")
+            split1 = str(cont).split('body_wrap">')[1]
+            split2 = split1.split('<div class="post_foot">')[0]
+            tree = split2.replace("\\n", "").replace("\\t", "").replace("  ", "").replace("&", "&amp;")
             tree = tree[:-12]
             parser = ET.XMLParser(encoding="utf-8")
             etree = ET.fromstring(tree, parser)
             for game in etree.findall(".//*[@class='product_row game first']"):
-               game_name,game_score,user_score = get_game_info(game)
-               list_games.append([game_name,game_score,user_score])
+                game_name, game_score, user_score = get_game_info(game)
+                list_games.append([game_name, game_score, user_score])
             for game in etree.findall(".//*[@class='product_row game']"):
-               game_name,game_score,user_score = get_game_info(game)
-               list_games.append([game_name,game_score,user_score])
+                game_name, game_score, user_score = get_game_info(game)
+                list_games.append([game_name, game_score, user_score])
             for game in etree.findall(".//*[@class='product_row game lastt']"):
-               game_name,game_score,user_score = get_game_info(game)
-               list_games.append([game_name,game_score,user_score])
-        page = page+1
+                game_name, game_score, user_score = get_game_info(game)
+                list_games.append([game_name, game_score, user_score])
+        page = page + 1
     return list_games
-   
+
+def get_list_of_games_howlongtobeat():
+    list_games = []
+    page = 1
+    last_page = False
+
+
+    while (last_page == False):
+        url = "https://howlongtobeat.com/search_main.php?page=" + str(page)
+        html_doc = post_to_html(url)
+        cont = html_doc.content
+
+        if str(cont).find("No results for ") >= 0:
+            last_page = True
+        else:
+
+            tree = "<root>"+cont.decode("utf-8").replace("&", "&amp;")+"</root>"
+            #print("Body:\n", str(tree))
+            parser = ET.XMLParser(encoding="utf-8")
+            etree = ET.fromstring(tree, parser)
+
+
+            for game in etree.findall(".//*[@class='back_dark shadow_box']"):
+                game_name, main_story, main_plus_extra, completionist = get_game_hltb_info(game)
+                list_games.append([game_name,  main_story, main_plus_extra, completionist])
+        page = page + 1
+    return list_games
+
+
+
 def get_game_info(game_element):
     """gameInfo = ET.tostring(game_element, encoding='utf8', method="xml")
     print(gameInfo)"""
@@ -72,31 +114,55 @@ def get_game_info(game_element):
     game_score = game_element.find(".//*[@class='product_item product_score']/div").text
     game_name = game_element.find(".//*[@class='product_item product_title']/a").text
     user_score_node = game_element.findall(".//*[@class='product_item product_userscore_txt']/span")
-    
+
     user_score = user_score_node[1].text
-    print("Game: ",game_name, " Score: ",game_score, " UserScore: ",user_score)
+    print("Game: ", game_name, " Score: ", game_score, " UserScore: ", user_score)
     if game_score == "tbd":
         game_score = "-"
     if user_score == "tbd":
         user_score = "-"
-    return game_name,game_score,user_score
-    
-def keep_trying_to_get_html(url, attempt=0):
-    logging.debug('[keep_trying_to_get_html] Making request to: ' + url)
-    try:
-        user_agent = {
-            'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    return game_name, game_score, user_score
 
-        html_doc = requests.get(url, headers=user_agent)
-        return html_doc
-    except:
-        if attempt > 3:
-            logging.error("Giving up on HTTP request: " + url)
-            raise Exception("Failed to fetch " + url)
-        logging.debug('[keep_trying_to_get_html] HTTP error on ' + url + '. Retrying...')
-        time.sleep(1)
-        print("Try ", attempt)
-        return keep_trying_to_get_html(url, attempt=attempt + 1)
+def get_game_hltb_info(game_element):
+
+    game_name = game_element.find(".//*[@class='text_white']").text
+    times = game_element.findall(".//*[@class='search_list_details_block']/div/div")
+    if len(times) == 0:
+        times = game_element.findall(".//*[@class='search_list_details_block']/div")
+    main_story = (times[1].text).replace("&#189;",".5").replace("Hours","").replace(" ","")
+    if len(times)>2:
+        main_plus_extra = (times[3].text).replace("&#189;",".5").replace("Hours","h").replace(" ","")
+        if len(times) > 4:
+            completionist = (times[5].text).replace("&#189;",".5").replace("Hours","h").replace(" ","")
+        else:
+            completionist = "-"
+    else:
+        main_plus_extra = "-"
+        completionist = "-"
+
+    print("Game: ", game_name, " main_story: ", main_story, " main_plus_extra: ", main_plus_extra, " completionist: ", completionist)
+
+    return game_name,  main_story, main_plus_extra, completionist
+
+def get_to_html(url):
+    user_agent = {
+        'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    html_doc = requests.get(url, headers=user_agent)
+    return html_doc
+
+
+def post_to_html(url):
+    logging.debug('[keep_trying_to_get_html] Making request to: ' + url)
+
+    headers = {'Content-type': 'application/x-www-form-urlencoded', 'Accept': '*/*',
+        'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    data = {'queryString': '','t': 'games', 'sorthead': 'popular', 'sortd': 'Normal Order','plat': 'Nintendo Switch',
+            'length_type': 'main', 'length_min':'', 'length_max':'', 'detail': ''    }
+    payload = "queryString=&t=games&sorthead=popular&sortd=Normal Order&plat=Nintendo Switch&length_type=main&length_min=&length_max=&detail="
+    html_doc = requests.post(url, headers=headers, data = payload)
+    return html_doc
+
 
 class App(tk.Frame):
     def __init__(self, master=None):
@@ -138,22 +204,29 @@ class App(tk.Frame):
         self.scrollbar = tk.Scrollbar(game_selection_frame)
         ##        self.scrollbar.grid(row=1, column=1, sticky=N+S+W)
         self.title_list = tk.Listbox(game_selection_frame, exportselection=False, \
-                                  yscrollcommand=self.scrollbar.set, selectmode='extended')
+                                     yscrollcommand=self.scrollbar.set, selectmode='extended')
         ##        self.title_list.grid(row=1, column=0, sticky=W)
         self.scrollbar.config(command=self.title_list.yview)
 
         # Setup Treeview and Two Scrollbars
         container = ttk.Frame(game_selection_frame)
         container.grid(row=1, column=0, columnspan=2)
-        self.tree = ttk.Treeview(columns=("num", "G", "S", "US"), show="headings", selectmode='extended')
+        self.tree = ttk.Treeview(columns=("num", "G", "S", "US", "TM", "TMP", "TC"), show="headings",
+                                 selectmode='extended')
         self.tree.heading("num", text="#", command=lambda c="num": self.sortby(self.tree, c, 0))
         self.tree.column("num", width=40)
         self.tree.heading("G", text="Game", command=lambda c="G": self.sortby(self.tree, c, 0))
-        self.tree.column("G", width=590)
+        self.tree.column("G", width=440)
         self.tree.heading("S", text="Score", command=lambda c="S": self.sortby(self.tree, c, 0))
         self.tree.column("S", width=50)
         self.tree.heading("US", text="User Score", command=lambda c="US": self.sortby(self.tree, c, 0))
         self.tree.column("US", width=50)
+        self.tree.heading("TM", text="Main Story", command=lambda c="TM": self.sortby(self.tree, c, 0))
+        self.tree.column("TM", width=50)
+        self.tree.heading("TMP", text="Main+Extras", command=lambda c="TMP": self.sortby(self.tree, c, 0))
+        self.tree.column("TMP", width=50)
+        self.tree.heading("TC", text="Completionist", command=lambda c="TC": self.sortby(self.tree, c, 0))
+        self.tree.column("TC", width=50)
 
         vsb = ttk.Scrollbar(orient="vertical", command=self.tree.yview)
         hsb = ttk.Scrollbar(orient="horizontal", command=self.tree.xview)
@@ -166,16 +239,18 @@ class App(tk.Frame):
 
         with open(fileOutName, encoding="utf8") as f:
             self.games = f.readlines()
-        i= 1
+        i = 1
         for game in self.games:
             number = i
             game_name = game.split("|")[0]
             score = game.split("|")[1]
             user_score = game.split("|")[2]
-            tree_row = (number, game_name, score, user_score)
+            main_story = game.split("|")[3]
+            main_plus_extra = game.split("|")[4]
+            completionist = game.split("|")[5]
+            tree_row = (number, game_name, score, user_score, main_story, main_plus_extra, completionist)
             self.tree.insert('', 'end', values=tree_row)
-            i = i+1
-
+            i = i + 1
 
     def sortby(self, tree, col, descending):
         """sort tree contents when a column header is clicked on"""
@@ -195,13 +270,16 @@ class App(tk.Frame):
 
         search_term = self.search_var.get()
         self.tree.delete(*self.tree.get_children())
-        i=1
+        i = 1
         for game in self.games:
             number = i
             game_name = game.split("|")[0]
             score = game.split("|")[1]
             user_score = game.split("|")[2]
-            tree_row = (number, game_name, score, user_score)
+            main_story = game.split("|")[3]
+            main_plus_extra = game.split("|")[4]
+            completionist = game.split("|")[5]
+            tree_row = (number, game_name, score, user_score, main_story, main_plus_extra, completionist)
             i = i + 1
             if search_term.lower() in game_name.lower():
                 self.tree.insert('', 'end', values=tree_row)
@@ -211,7 +289,12 @@ class App(tk.Frame):
         self.tree.heading("num", text="#", command=lambda c="num": self.sortby(self.tree, c, 1))
         self.tree.heading("G", text=("Game"), command=lambda c="G": self.sortby(self.tree, c, 1))
         self.tree.heading("S", text=("Score"), command=lambda c="S": self.sortby(self.tree, c, 1))
-        self.tree.heading("US", text=("User Score"), command=lambda c="S": self.sortby(self.tree, c, 1))
+
+        self.tree.heading("TM", text=("Main Story"), command=lambda c="S": self.sortby(self.tree, c, 1))
+        self.tree.heading("TMP", text=("Main+Extras"), command=lambda c="S": self.sortby(self.tree, c, 1))
+        self.tree.heading("TC", text=("Completionist"), command=lambda c="S": self.sortby(self.tree, c, 1))
+
+
 try:
     with open(titleKeysFile, encoding="utf8") as f:
         lines = f.readlines()
@@ -222,22 +305,26 @@ except Exception as e:
 
 if not os.path.exists(fileOutName):
     print("Getting scores")
-    list_games = get_list_of_games()
+    list_games_scores = get_list_of_games()
+    print("Getting times")
+    list_games_times = get_list_of_games_howlongtobeat()
     print("Creating " + fileOutName)
     try:
         fOut = open(fileOutName, 'w', encoding="utf-8")
         for line in lines:
             if line.strip() != '':
                 game = line.split("|")[2].replace("\n", "")
-                #game_url = "".join([x if ord(x) < 128 else '' for x in game])
-                score, user_score = get_game_scores(game, list_games)
-                fOut.write(game + "|" + score + "|" + user_score +  "\n")
+                # game_url = "".join([x if ord(x) < 128 else '' for x in game])
+                score, user_score = get_game_scores(game, list_games_scores)
+                main_story, main_plus_extra, completionist = get_game_times(game, list_games_times)
+                fOut.write(
+                    game + "|" + score + "|" + user_score + "|" + main_story + "|" + main_plus_extra + "|" + completionist + "\n")
         fOut.close()
     except Exception as e:
         print("Error:", e)
         os.system("pause")
 else:
-    print("File "+fileOutName+" found")
+    print("File " + fileOutName + " found")
 
 root = tk.Tk()
 app = App(root)
